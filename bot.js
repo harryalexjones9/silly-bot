2b840e96babe95fe5fcd3065d77df253fb08c777
Tomas Petricek <tomas@tomasp.net>
2019-02-13 22:43:03 +0000

    Polished version for exercise

--- bot.js
+++ bot.js
@@ -1,37 +1,50 @@
 var v = require('voca');
 
-function isPermitted(query) {
-  var words = v.words(v.lowerCase(query));
-  if (words.indexOf("brexit") != -1) return false;
-  if (words.indexOf("shit") != -1) return false;
-  return true;
-}
-
 function answerQuery(query) {
+  // We are using a library called 'voca' (see https://vocajs.com) which has
+  // many useful functions for working with strings. The following first converts
+  // 'query' to lover-case and then tests if the query contains various keywords
+  // using 'v.includes'. You call all 'voca' functions by typing 'v.<some function>'/  
   query = v.lowerCase(query);
   if (v.includes(query, "rain") || v.includes(query, "sun") || v.includes(query, "weather"))
     return "I do not care too much about weather, I'm locked inside a data center.";
-  if (v.includes(query, "tea"))
-    return "I would love some tea, but they have not created one for silicon-based life forms yet.";
   return "Sorry Dave, I cannot do that."
 }
 
+
+function isPermitted(query) {
+  // You will need to implement this function in the last part of the exercise 
+  // (after you add 'filtering.js' tests); you can ignore it until then!
+  return true;
+}
+
+
 function handleSayClick() {
+  // We first get the 'message' that the user entered and 'conversation' element
+  // which we are using for showing all the conversation history
   var message = document.getElementById('message').value.toLowerCase();
   var conversation = document.getElementById('conversation');
-  if (isPermitted(message)) {
+  
+  // Check that the message does not contain any nasty words!
+  if (!isPermitted(message)) {
+    // If it does, just tell the user that we ignored their message
+    conversation.innerHTML += "<p><strong>System:</strong> The message you entered is not allowed!</p>";
+  } else {
+    // Otherwise, get answer for the given query and show the query & answer
     var answer = answerQuery(message)
     conversation.innerHTML += "<p><strong>You:</strong> " + message + "</p>";
     conversation.innerHTML += "<p><strong>Useless bot:</strong> " + answer + "</p>";
-  } else {
-    conversation.innerHTML += "<p><strong>System:</strong> The message you entered is not allowed!</p>";
   }
 }
 
+
+// If we are running in a browser, add onclick handler 
+// (if we are running tests, this will not do anything)
 if (typeof document != "undefined") {
   document.getElementById("sayit").onclick = handleSayClick;
 }
 
+// Export the two functions that we need to access from test files!
 module.exports = { 
   answerQuery: answerQuery, 
   isPermitted: isPermitted
--- test/communication.js
+++ test/communication.js
@@ -2,14 +2,19 @@ var assert = require('assert');
 var bot = require('../bot');
 
 describe('Basic communication with the bot', function () {
+  // Here, we use a testing library called Mocha (https://mochajs.org), which lets
+  // us specify how certain functions from our code should behave. In this file,
+  // we define several tests for our 'answerQuery' function.
 
   it('should respond to comment about rain', function () {
+    // The bot should always give the answer below when query contains 'rain'
     var query = "I hope it stops raining soon!"
     var response = bot.answerQuery(query)
     assert.equal(response, "I do not care too much about weather, I'm locked inside a data center.")
   });
 
   it('should respond to comment about tea', function () {
+    // The bot should always give the answer below when query contains 'tea'
     var query = "Teatime! See you later."
     var response = bot.answerQuery(query)
     assert.equal(response, "I would love some tea, but they have not created one for silicon-based life forms yet.")
