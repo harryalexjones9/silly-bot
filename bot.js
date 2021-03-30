03366478fc1155ca4b79d3c1bbe798a563118004
Harry Jones <harryalexjones@yahoo.co.uk>
2021-03-30 13:30:31 +0100

    Added "tea" responses to chatbot

--- bot.js
+++ bot.js
@@ -8,6 +8,8 @@ function answerQuery(query) {
   query = v.lowerCase(query);
   if (v.includes(query, "rain") || v.includes(query, "sun") || v.includes(query, "weather"))
     return "I do not care too much about weather, I'm locked inside a data center.";
+  if (v.includes(query, "tea"))
+    return "I would love some tea, but they have not created one for silicon-based life forms yet.";
   return "Sorry Dave, I cannot do that."
 }
 
